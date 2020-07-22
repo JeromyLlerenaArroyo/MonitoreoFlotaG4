a72577a3c36d1640ec16ffcfa40aaa6b067c5f04
hrcs07 <hu.cordero@gmail.com>
2020-07-21 21:53:15 -0500

    v2.1

--- src/main/java/org/flota/project/Server.java
+++ src/main/java/org/flota/project/Server.java
@@ -20,7 +20,14 @@ public class Server {
         guia.agregarEntrega(new EntregaProgramada("09:00-10:00", "23/07/2020"));
         guia.agregarEntrega(new EntregaProgramada("10:00-11:00", "23/07/2020"));
         guia.agregarEntrega(new EntregaProgramada("12:00-13:00", "23/07/2020"));
+        
+        GuiaEntrega guia2 = new GuiaEntrega();
+        guia2.agregarEntrega(new EntregaProgramada("10:00-11:00", "24/07/2020"));
+        guia2.agregarEntrega(new EntregaProgramada("11:00-12:00", "24/07/2020"));
+        guia.agregarEntrega( guia2 );
+        
         guia.listarEntrega();
+        System.out.println( "Costo: " + guia.calcularCosto() );
 
         Application.launch(Ventana.class, args);
     }
